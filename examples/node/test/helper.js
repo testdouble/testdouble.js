fcb0dafb8d9e6179d1f10623aa59cbc7d7f3e52e
Justin Searls <searls@gmail.com>
2016-04-03 12:09:08 -0400

    Get tests running in 0.10.x

--- examples/node/test/helper.js
+++ examples/node/test/helper.js
@@ -2,6 +2,12 @@ global.expect = require('chai').expect;
 global.context = describe;
 global.td = require('testdouble');
 
+var version = require('semver')(process.version);
+if(version.major == 0 && version.minor < 11) {
+  console.warn("SKIPPING TESTS: This example requires Node.js 0.11 or higher.");
+  process.exit(0);
+}
+
 afterEach(function(){
   td.reset();
 });
