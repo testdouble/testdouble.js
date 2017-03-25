e91ae445275990a316a41d23254445d3c10a5d86
Justin Searls <searls@gmail.com>
2017-03-25 17:39:42 -0400

    make it more carefully only resolve as a fallback
    i forgot resolve blows up when it can't resolve. may as well only call
    it when the existing code path fails

--- src/replace/module.js
+++ src/replace/module.js
@@ -6,8 +6,18 @@ quibble.ignoreCallsFromThisFile()
 
 export default function (path, stub) {
   if (arguments.length > 1) { return quibble(path, stub) }
-  const realThing = require(resolve.sync(quibble.absolutify(path), {basedir: process.cwd()}))
+  const realThing = requireAt(path)
   const fakeThing = imitate(realThing, path)
   quibble(path, fakeThing)
   return fakeThing
+}
+
+var requireAt = (path) => {
+  try {
+    // 1. Try just following quibble's inferred path
+    return require(quibble.absolutify(path))
+  } catch (e) {
+    // 2. Try including npm packages
+    return require(resolve.sync(path, { basedir: process.cwd() }))
+  }
 }
