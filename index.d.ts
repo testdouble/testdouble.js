bc203628293ea516a1e7786a2631182aa4956523
Brian Ojeda <bojeda@hotmail.com>
2017-05-04 10:13:34 -0400

    Organize and add func() support

--- index.d.ts
+++ index.d.ts
@@ -1,51 +1,17 @@
-export type TestDouble = Function;
 
-export type DoubledObjectWithKey<Key extends string> = {};
+//
+// types and interfaces
+// ----------------------------------------------------------------------------
 
 export type DoubledObject<Subject> = Subject;
 
-export interface TestdoubleConfig {
-  promiseConstructor?: any;
-  ignoreWarnings?: boolean;
-  suppressErrors?: boolean;
-}
-export function config(config: TestdoubleConfig): void;
+export type DoubledObjectWithKey<Key extends string> = {};
 
-declare function functionDouble(name?: string): TestDouble;
-export { functionDouble as function };
+export type TestDouble<Function> = Function;
 
-// When passed class or constructor function
-export function object<T>(constructor: { new (...args: any[]): T }): DoubledObject<T>;
-
-// When passed array of props
-export function object<Key extends string>(props: Key[]): DoubledObjectWithKey<Key>;
-
-// When passed class or constuctor function name as string value
-export function object<T>(object: string): DoubledObject<T>;
-
-// When passed general object
-export function object<T>(object: T): DoubledObject<T>;
-
-export interface Stubber {
-  thenReturn(...args: any[]): TestDouble;
-  thenDo(f: Function): TestDouble;
-  thenThrow(e: Error): TestDouble;
-  thenResolve(v: any): TestDouble;
-  thenReject(e: Error): TestDouble;
-  thenCallback(...args: any[]): TestDouble;
-}
-
-export function callback(...args: any[]): void;
-
-export function when(...args: any[]): Stubber;
-
-export interface Matchers {
-  anything(): any;
-  isA(type: Function): any;
-  contains(a: string|any[]|{}): any;
-  argThat(matcher: Function): any;
-  not(v: any): any;
-  captor(): Captor
+interface Call {
+  context: {};
+  args: any[];
 }
 
 export interface Captor {
@@ -54,29 +20,91 @@ export interface Captor {
   values?: any[];
 }
 
-export const matchers: Matchers;
-
-export function replace(path: string, f?: any): any;
-export function replace(path: {}, property: string, f?: any): any;
-
-export function reset(): void;
-
-export interface VerificationConfig {
-  ignoreExtraArgs?: boolean;
-  times?: number;
-}
-
-export function verify(a: any, check?: VerificationConfig): void;
-
-interface Call {
-  context: {};
-  args: any[];
-}
-
 export interface Explanation {
   callCount: number;
   calls: Call[];
   description: string;
 }
 
-export function explain(f: TestDouble): Explanation;
+export interface Matchers {
+  anything(): any;
+  isA(type: Function): any;
+  contains(a: string | any[] | {}): any;
+  argThat(matcher: Function): any;
+  not(v: any): any;
+  captor(): Captor
+}
+
+export const matchers: Matchers;
+
+export interface Stubber {
+  thenReturn<T>(...args: any[]): TestDouble<T>;
+  thenDo<T>(f: Function): TestDouble<T>;
+  thenThrow<T>(e: Error): TestDouble<T>;
+  thenResolve<T>(v: any): TestDouble<T>;
+  thenReject<T>(e: Error): TestDouble<T>;
+  thenCallback<T>(...args: any[]): TestDouble<T>;
+}
+
+export interface TestdoubleConfig {
+  promiseConstructor?: any;
+  ignoreWarnings?: boolean;
+  suppressErrors?: boolean;
+}
+
+export interface VerificationConfig {
+  ignoreExtraArgs?: boolean;
+  times?: number;
+}
+
+//
+// general
+// ----------------------------------------------------------------------------
+
+export function config(config: TestdoubleConfig): void;
+
+export function reset(): void;
+
+export function explain<T>(f: TestDouble<T>): Explanation;
+
+
+//
+// fake: constructors
+// ----------------------------------------------------------------------------
+
+//
+// fake: functions
+// ----------------------------------------------------------------------------
+
+declare function functionDouble(name?: string): TestDouble<Function>;
+
+declare function functionDouble<T>(name?: T): TestDouble<T>;
+
+export { functionDouble as function };
+export { functionDouble as func };
+
+//
+// fake: objects
+// ----------------------------------------------------------------------------
+
+export function object<T>(constructor: { new (...args: any[]): T }): DoubledObject<T>;
+
+export function object<Key extends string>(props: Key[]): DoubledObjectWithKey<Key>;
+
+export function object<T>(object: string): DoubledObject<T>;
+
+export function object<T>(object: T): DoubledObject<T>;
+
+//
+// stubbing
+// ----------------------------------------------------------------------------
+
+export function callback(...args: any[]): void;
+
+export function replace(path: string, f?: any): any;
+
+export function replace(path: {}, property: string, f?: any): any;
+
+export function when(...args: any[]): Stubber;
+
+export function verify(a: any, check?: VerificationConfig): void;
--- test/src/typescript/test.ts
+++ test/src/typescript/test.ts
@@ -1,5 +1,18 @@
 import * as td from "../../../";
 
+// dummies
+
+class Dog {
+  constructor() {};
+  bark(): string { return "bark! bark!" }
+}
+
+function sum (first: number, second: number): number {
+  return first + second
+}
+
+// td.functions()
+
 const f = td.function();
 td.when(f(10)).thenReturn(10);
 td.when(f(1)).thenThrow(new Error("ok"));
@@ -7,29 +20,52 @@ td.when(f(td.matchers.isA(String))).thenDo(function(s: string) { return s; });
 td.when(f(td.matchers.not(true))).thenResolve("value");
 td.when(f(td.matchers.not(false))).thenReject(new Error("rejected"));
 
-class Dog {
-  bark() {}
-}
+const fakeSum = td.function(sum);
+td.when(fakeSum(1, 2)).thenReturn(3);
+
+const fakerSum = td.function("sum");
+td.when(fakerSum(1, 2)).thenReturn(3);
+
+const fakestSum = td.function("sum");
+td.when(fakestSum(1, 2)).thenReturn(3);
+
+// td.func()
+
+const ff = td.func();
+td.when(ff(10)).thenReturn(10);
+td.when(ff(1)).thenThrow(new Error("ok"));
+td.when(ff(td.matchers.isA(String))).thenDo(function(s: string) { return s; });
+td.when(ff(td.matchers.not(true))).thenResolve("value");
+td.when(ff(td.matchers.not(false))).thenReject(new Error("rejected"));
+
+// td.constructor()
 
 const dog = td.constructor(Dog);
 td.when(dog.bark()).thenReturn("bark!");
 
+// td.object()
+
 const bird = td.object({ fly: function(){} });
 td.when(bird.fly()).thenReturn("fly!");
 
-class Bear { constructor() {}; sleep() {}; };
-const bear = td.object<Bear>("Bear");
-td.when(bear.sleep()).thenReturn("zzz");
+const bear = td.object<Dog>("Bear");
+td.when(bear.bark()).thenReturn("bark!");
+
+// td.replace()
 
 td.replace({}, "prop");
 td.replace({}, "prop", 42);
 td.replace("../../..");
 td.replace("../../../", 42);
 
+// td.verify()
+
 td.verify(f());
 td.verify(f(), { times: 1 });
 td.verify(f(), { ignoreExtraArgs: false });
 td.verify(f(), { ignoreExtraArgs: true, times: 2 });
+
+// td.explain()
 
 const explanation = td.explain(f);
 console.log(
