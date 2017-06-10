60202e7c0b635a4030d96f088202fd500dd58342
Justin Searls <searls@gmail.com>
2017-06-10 11:51:02 -0400

    Reimplement the copy-properties function
    * Use more ES-like semantics (e.g. it's a void func that does not return
    showing it's a side effect instead of being an expression)
    * Write tests
    
    TODO: I'm unsure why I made configurable/writable always true in the
    original impl, but i'm not about to change it now

--- src/function/imitate/copy-props.js
+++ src/function/imitate/copy-props.js
@@ -1,1 +1,14 @@
-export default () => {}
+import _ from '../../wrap/lodash'
+
+export default (original, target) => {
+  const descriptors = _.transform(Object.getOwnPropertyNames(original), (acc, name) => {
+    if (target.hasOwnProperty(name)) return
+    acc[name] = {
+      configurable: true,
+      writable: true,
+      value: original[name],
+      enumerable: Object.propertyIsEnumerable(original, name)
+    }
+  })
+  Object.defineProperties(target, descriptors)
+}
