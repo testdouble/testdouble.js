754fd79a7935c733144f9431dbc255e901970933
Justin Searls <searls@gmail.com>
2017-06-24 08:58:50 -0400

    Add a silly deeply nested example

--- examples/babel/lib/calculator.js
+++ examples/babel/lib/calculator.js
@@ -1,6 +1,7 @@
 import Adder from './adder'
 import Subtractor from './subtractor'
 import Multiplier from './multiplier'
+import divider from './divider'
 
 export default class Calculator {
   calculate(operation, ...operands) {
@@ -11,6 +12,10 @@ export default class Calculator {
         return new Subtractor().subtract(operands[0], operands[1])
       case 'multiply':
         return new Multiplier().multiply(operands[0], operands[1])
+      case 'divide':
+        divider.numerator.set(operands[0])
+        divider.otherOptions.divisor.setThatToo(operands[1])
+        return new divider.ohAndAlso.Executor().execute()
     }
   }
 }
--- examples/babel/test/lib/calculator-test.js
+++ examples/babel/test/lib/calculator-test.js
@@ -1,11 +1,12 @@
 import assert from 'assert'
 
-let subject, Adder, Subtractor, Multiplier, result
+let subject, Adder, Subtractor, Multiplier, divider, result
 describe('Calculator', () => {
   beforeEach(() => {
     Adder = td.replace('../../lib/adder')
     Subtractor = td.replace('../../lib/subtractor')
     Multiplier = td.replace('../../lib/multiplier').default
+    divider = td.replace('../../lib/divider').default
     const Calculator = require('../../lib/calculator').default
     subject = new Calculator
   })
@@ -35,6 +36,18 @@ describe('Calculator', () => {
       result = subject.calculate('multiply', 1, 4)
 
       assert.equal(result, 'times math!')
+    })
+
+    it('delegates to a really weird and very deeply nested Divider', () => {
+      // This is a very bad test to verify deep nesting.
+      // Please do not write things like this!
+      td.when(divider.ohAndAlso.Executor.prototype.execute()).thenReturn('lol')
+
+      result = subject.calculate('divide', 10, 2)
+
+      td.verify(divider.numerator.set(10))
+      td.verify(divider.otherOptions.divisor.setThatToo(2))
+      assert.equal(result, 'lol')
     })
   })
 })
