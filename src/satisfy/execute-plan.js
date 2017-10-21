66fb47ca2553efb7c8b934731c914769adb0d2b1
Justin Searls <searls@gmail.com>
2017-10-21 16:03:43 -0400

    think about this

--- src/satisfy/execute-plan.js
+++ src/satisfy/execute-plan.js
@@ -1,3 +1,12 @@
-export default function executePlan () {
+export default function executePlan (double, call, stubbing) {
+  // 1. get stubbed value
+  // 2. stubbing.incrementSatisfactions()
+  // 2a. invoke any callback matchers
+  // 3. switch stubbing.type
+    // case 'thenReturn': return value
+    // case 'thenDo': return value.apply(actualContext, actualArgs)
+    // case 'thenThrow': throw value
+    // case 'thenResolve': return createPromise(stubbing, value, true)
+    // case 'thenReject': return createPromise(stubbing, value, false)
 }
 
