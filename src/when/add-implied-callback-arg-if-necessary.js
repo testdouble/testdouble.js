40646745d15695fe3fb22247a38c84351e58529f
Justin Searls <searls@gmail.com>
2017-10-21 11:41:27 -0400

    #rewrite wrap the when/* children

--- src/when/add-implied-callback-arg-if-necessary.js
+++ src/when/add-implied-callback-arg-if-necessary.js
@@ -1,2 +1,12 @@
+import _ from '../wrap/lodash'
+
+import isCallback from '../matchers/is-callback'
+import callback from '../callback'
+
 export default function (type, args) {
+  if (type === 'thenCallback' && !_.some(args, isCallback)) {
+    return args.concat(callback)
+  } else {
+    return args
+  }
 }
--- test/unit/when/add-implied-callback-arg-if-necessary.test.js
+++ test/unit/when/add-implied-callback-arg-if-necessary.test.js
@@ -1,3 +1,28 @@
+let callback, isCallback, subject
 module.exports = {
+  beforeEach: () => {
+    callback = td.replace('../../../src/callback').default
+    isCallback = td.replace('../../../src/matchers/is-callback').default
 
+    subject = require('../../../src/when/add-implied-callback-arg-if-necessary').default
+  },
+  'type is not thenCallback': () => {
+    const result = subject('anything', [{}, 50])
+
+    assert.deepEqual(result, [{}, 50])
+  },
+  'type is thenCallback and no callback matcher is in the args': () => {
+    td.when(isCallback(), {ignoreExtraArgs: true}).thenReturn(false)
+
+    const result = subject('thenCallback', [42, 'pants'])
+
+    assert.deepEqual(result, [42, 'pants', callback])
+  },
+  'type is thenCallback and a callback matcher IS in the args': () => {
+    td.when(isCallback('a callback'), {ignoreExtraArgs: true}).thenReturn(true)
+
+    const result = subject('thenCallback', [42, 'pants', 'a callback'])
+
+    assert.deepEqual(result, [42, 'pants', 'a callback'])
+  }
 }
