fb0d36c566dd7d0a73242352ef2a4c842ce4c96f
Justin Searls <searls@gmail.com>
2017-12-20 09:42:10 -0500

    Update the captor matcher to use the new hook

--- src/matchers/builtin/captor.js
+++ src/matchers/builtin/captor.js
@@ -5,10 +5,12 @@ export default () => {
     capture: create({
       name: 'captor.capture',
       matches (matcherArgs, actual) {
+        return true
+      },
+      afterSatisfaction (matcherArgs, actual) {
         captor.values = captor.values || []
         captor.values.push(actual)
         captor.value = actual
-        return true
       }
     })
   }
