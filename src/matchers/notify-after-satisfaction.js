47dcb9a56956eb99addd2a3226f185b54221f35e
Justin Searls <searls@gmail.com>
2017-12-20 11:39:43 -0500

    update rewrite with new edge case

--- src/matchers/notify-after-satisfaction.js
+++ src/matchers/notify-after-satisfaction.js
@@ -1,6 +1,7 @@
 import _ from '../wrap/lodash'
 import isMatcher from './is-matcher'
 
+// TODO: after rewrite, update signature to take (Stubbing/Verification, Call)
 export default function notifyAfterSatisfaction (expectedArgs, actualArgs) {
   _.each(expectedArgs, (expectedArg, i) => {
     if (isMatcher(expectedArg)) {
