389a2999f2fdd0ffca9fa1baa6fea23f0f45555c
Justin Searls <searls@gmail.com>
2017-12-22 10:52:18 -0500

    add a test for clarity

---
+++ test/unit/imitate/overwrite-children/is-native-prototype.test.js
@@ -0,0 +1,28 @@
+import subject from '../../../../src/imitate/overwrite-children/is-native-prototype'
+
+module.exports = {
+  'passing junk': () => {
+    assert.strictEqual(subject(null), false)
+    assert.strictEqual(subject(undefined), false)
+  },
+  'passing built-in types': () => {
+    assert.strictEqual(subject("hi"), false)
+    assert.strictEqual(subject(5), false)
+    assert.strictEqual(subject(Object), false)
+    assert.strictEqual(subject(new Object()), false)
+    assert.strictEqual(subject(Object.create({})), false)
+    assert.strictEqual(subject({}), false)
+    assert.strictEqual(subject(Function), false)
+    assert.strictEqual(subject(function () {}), false)
+  },
+  'passing prototypes of built-in types': () => {
+    assert.strictEqual(subject(("hi").prototype), false)
+    assert.strictEqual(subject((5).prototype), false)
+    assert.strictEqual(subject(Object.prototype), true)
+    assert.strictEqual(subject((new Object()).prototype), false)
+    assert.strictEqual(subject(Object.create({}).prototype), false)
+    assert.strictEqual(subject(({}).prototype), false)
+    assert.strictEqual(subject(Function.prototype), true)
+    assert.strictEqual(subject((function () {}).prototype), false)
+  }
+}
