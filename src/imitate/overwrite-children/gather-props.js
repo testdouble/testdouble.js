b8c0dbfc83023d1378a9a42672891b3e70b614f1
Justin Searls <searls@gmail.com>
2017-12-22 11:14:15 -0500

    standardize

--- src/imitate/overwrite-children/gather-props.js
+++ src/imitate/overwrite-children/gather-props.js
@@ -13,4 +13,3 @@ export default function gatherProps (thing) {
   }
   return props
 }
-
--- test/unit/imitate/overwrite-children/is-native-prototype.test.js
+++ test/unit/imitate/overwrite-children/is-native-prototype.test.js
@@ -6,23 +6,21 @@ module.exports = {
     assert.strictEqual(subject(undefined), false)
   },
   'passing built-in types': () => {
-    assert.strictEqual(subject("hi"), false)
+    assert.strictEqual(subject('hi'), false)
     assert.strictEqual(subject(5), false)
     assert.strictEqual(subject(Object), false)
-    assert.strictEqual(subject(new Object()), false)
     assert.strictEqual(subject(Object.create({})), false)
     assert.strictEqual(subject({}), false)
     assert.strictEqual(subject(Function), false)
     assert.strictEqual(subject(function () {}), false)
   },
   'passing prototypes of built-in types': () => {
-    assert.strictEqual(subject(("hi").prototype), false)
+    assert.strictEqual(subject(('hi').prototype), false)
     assert.strictEqual(subject((5).prototype), false)
     assert.strictEqual(subject(Object.prototype), true)
-    assert.strictEqual(subject((new Object()).prototype), false)
     assert.strictEqual(subject(Object.create({}).prototype), false)
     assert.strictEqual(subject(({}).prototype), false)
     assert.strictEqual(subject(Function.prototype), true)
-    assert.strictEqual(subject((function () {}).prototype), false)
+    assert.strictEqual(subject(function () {}.prototype), false)
   }
 }
