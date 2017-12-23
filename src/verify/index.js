d9d30f9dc80ac7263c482f7bfe63a9235a4031ba
Justin Searls <searls@gmail.com>
2017-12-23 09:08:56 -0500

    Step 4: make the first test pass
    This is not proper Kent Beckian triangulation, because 
    I threw the if() statement in there before it was properly
    required by the test. In practice I'll probably comment out
    the branch with and without the tests to ensure it's being
    demanded properly, which is hard to capture in a git log

--- src/verify/index.js
+++ src/verify/index.js
@@ -1,9 +1,17 @@
+import CallLog from '../value/call-log'
+
+import ensureDemonstration from './ensure-demonstration'
+import didCallOccur from './did-call-occur'
+import notifySatisfiedMatchers from './notify-satisfied-matchers'
+import warnIfAlsoStubbed from './warn-if-also-stubbed'
+
 export default function verify (__userInvokesDemonstrationHere__, config) {
-  // 1. double, call = CallLog.instance.pop
-  // 2. ensureDemonstration(call)
-  // 3. if call-occurred (double, call, config)
-  //   a. notify-satisfied-matchers(double, call, config)
-  //   b. warn-if-also-stubbed (double, call)
+  const {double, call} = CallLog.instance.pop()
+  ensureDemonstration(call)
+  if (didCallOccur(double, call, config)) {
+    notifySatisfiedMatchers(double, call, config)
+    warnIfAlsoStubbed(double, call, config)
+  }
   // 4. else
   //   a. fail-verification (double, call, config)
 }
