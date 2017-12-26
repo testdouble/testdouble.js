afb5ddf76ac957043bbda8898239cbbee57e2a62
Justin Searls <searls@gmail.com>
2017-12-26 09:46:53 -0500

    rename and extract private methods

--- src/matchers/builtin/contains.js
+++ src/matchers/builtin/contains.js
@@ -7,30 +7,34 @@ export default create({
   matches (containings, actualArg) {
     if (containings.length === 0) return false
 
-    return _.every(containings, (containing) => {
-      if (_.isArray(containing)) {
-        return _.some(actualArg, actualElement => _.isEqual(actualElement, containing))
-      } else if (isMatcher(containing)) {
-        return _.some(actualArg, containing.__matches)
-      } else if (_.isRegExp(containing)) {
-        return containing.test(actualArg)
-      } else if (_.isObjectLike(containing) && _.isObjectLike(actualArg)) {
-        return containsAllSpecified(containing, actualArg)
-      } else {
-        return _.includes(actualArg, containing)
-      }
-    })
+    return _.every(containings, (containing) =>
+      argumentContains(containing, actualArg)
+    )
   }
 })
 
-var containsAllSpecified = (containing, actual) => {
+const argumentContains = function (containing, actualArg) {
+  if (_.isArray(containing)) {
+    return _.some(actualArg, actualElement => _.isEqual(actualElement, containing))
+  } else if (_.isRegExp(containing)) {
+    return containing.test(actualArg)
+  } else if (isMatcher(containing)) {
+    return _.some(actualArg, containing.__matches)
+  } else if (_.isObjectLike(containing) && _.isObjectLike(actualArg)) {
+    return containsPartialObject(containing, actualArg)
+  } else {
+    return _.includes(actualArg, containing)
+  }
+}
+
+var containsPartialObject = (containing, actual) => {
   return actual != null && _.every(containing, (val, key) => {
     if (isMatcher(val)) {
       return val.__matches(actual[key])
     } else if (_.isArray(containing)) {
-      return containsAllSpecified(val, actual)
+      return containsPartialObject(val, actual)
     } else if (_.isObjectLike(val)) {
-      return containsAllSpecified(val, actual[key])
+      return containsPartialObject(val, actual[key])
     } else {
       return _.isEqual(val, actual[key])
     }
