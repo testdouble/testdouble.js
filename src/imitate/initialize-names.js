dc5c4d29ebf4248bc938b513795b420bb73ad9c1
Justin Searls <searls@gmail.com>
2018-02-10 15:08:44 -0500

    Publicize td.imitate()
    Doing this in support of a testdouble-jest module

--- src/imitate/initialize-names.js
+++ src/imitate/initialize-names.js
@@ -1,6 +1,7 @@
 import _ from '../wrap/lodash'
 
 export default (original, names) => {
+  if (_.isString(names)) return [names]
   if (names != null) return names
   if (_.isFunction(original) && original.name) {
     return [original.name]
