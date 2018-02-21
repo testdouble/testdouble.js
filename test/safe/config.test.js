14b5732ad7cfb38b7e7ba0c79e6f2fcb1c0ac653
Justin Searls <searls@gmail.com>
2018-02-21 14:05:53 -0500

    port config test

--- test/safe/config.test.js
+++ test/safe/config.test.js
@@ -1,43 +1,44 @@
-describe('td.config', () => {
-  it('sets some ok defaults', () => {
-    expect(td.config()).to.deep.equal({
+module.exports = {
+  'sets some ok defaults': () => {
+    assert.deepEqual(td.config(), {
       ignoreWarnings: false,
       promiseConstructor: global.Promise,
       suppressErrors: false
     })
-  })
-
-  it('overriding a real property', () => {
+  },
+  'overriding a real property': () => {
     const config = td.config({ignoreWarnings: true})
 
-    expect(config.ignoreWarnings).to.eq(true)
-    expect(td.config().ignoreWarnings).to.eq(true)
-  })
-
-  it('overriding a deprecated property', () => {
+    assert._isEqual(config.ignoreWarnings, true)
+    assert._isEqual(config.ignoreWarnings, true)
+    assert._isEqual(td.config().ignoreWarnings, true)
+  },
+  'overriding a deprecated property': () => {
     const ogWarn = console.warn
     const warnings = []
     console.warn = function (warning) { warnings.push(warning) }
 
     const config = td.config({extendWhenReplacingConstructors: true})
 
-    expect(config.extendWhenReplacingConstructors).to.eq(undefined)
-    expect(td.config().extendWhenReplacingConstructors).to.eq(undefined)
-    expect(warnings[0]).to.eq(
+    assert._isEqual(config.extendWhenReplacingConstructors, undefined)
+    assert._isEqual(td.config().extendWhenReplacingConstructors, undefined)
+    assert._isEqual(warnings[0],
       'Warning: testdouble.js - td.config - "extendWhenReplacingConstructors" is no longer a valid configuration key. Remove it from your calls to td.config() or it may throw an error in the future. For more information, try hunting around our GitHub repo for it:\n\n  https://github.com/testdouble/testdouble.js/search?q=extendWhenReplacingConstructors'
     )
 
     console.warn = ogWarn
-  })
-
-  it('overriding a non-existent property', () => {
+  },
+  'overriding a non-existent property': () => {
     let error
 
     try { td.config({wat: 'wat?'}) } catch (e) { error = e }
 
-    expect(error.message).to.eq(
+    assert._isEqual(error.message,
       'Error: testdouble.js - td.config - "wat" is not a valid configuration ' +
       'key (valid keys are: ["ignoreWarnings", ' +
       '"promiseConstructor", "suppressErrors"])')
-  })
-})
+  },
+  afterEach: () => {
+    td.config.reset()
+  }
+}
