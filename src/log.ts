533af4b061c0bd2201a3c1684113193456e893ed
Kevin Groat <apps@kevingroat.io>
2018-03-27 18:43:00 -0400

    Added typings to log.ts

--- src/log.ts
+++ src/log.ts
@@ -1,24 +1,24 @@
 import config from './config'
 
 export default {
-  warn (func, msg, url) {
+  warn (func: string, msg: string, url?: string) {
     if (!config().ignoreWarnings && typeof console === 'object' && console.warn) {
       console.warn(`Warning: testdouble.js - ${func} - ${msg}${withUrl(url)}`)
     }
   },
 
-  error (func, msg, url) {
+  error (func: string, msg: string, url?: string) {
     if (!config().suppressErrors) {
       throw new Error(`Error: testdouble.js - ${func} - ${msg}${withUrl(url)}`)
     }
   },
 
-  fail (msg) {
+  fail (msg: string) {
     throw new Error(msg)
   }
 }
 
-var withUrl = (url) =>
+var withUrl = (url?: string) =>
   url != null
     ? ` (see: ${url} )`
     : ''
