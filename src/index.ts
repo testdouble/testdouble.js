7c4188a2f99d873e5215061817cb07f1239a15f6
Justin Searls <searls@gmail.com>
2018-04-01 10:58:17 -0400

    Get index test passing
    1. import & export quibble (I can't figure out how to one-liner this)
    
    2. Remove the three names that are exported as undefined because I 
    can't figure out what to do with them. Hopefully someone in #346 can
    educate me!

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
+import * as quibble from 'quibble'
+
 export { default as function, default as func } from './function'
 export { default as object } from './object'
 export { default as constructor } from './constructor'
 export { default as imitate } from './imitate'
 export { default as when } from './when'
 export { default as verify } from './verify'
-export { default as matchers, Captor, Created } from './matchers'
+export { default as matchers } from './matchers'
 export { default as replace } from './replace'
 export { default as explain } from './explain'
 export { default as reset } from './reset'
-export { default as config, ConfigObject } from './config'
+export { default as config } from './config'
 export { default as callback } from './callback'
 export { default as version } from './version'
-export { * as quibble } from 'quibble'
+export { quibble }
+
