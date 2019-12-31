de6683aebf50c1fb84ac3a4d7b4eefebeaa78be2
Mohammad Hadi Aliakbar <hadi.aliakbar@gmail.com>
2019-12-31 18:00:34 +0330

    fix typescript test eslint

--- test/safe/typescript-typings.test.ts
+++ test/safe/typescript-typings.test.ts
@@ -1,102 +1,102 @@
-import * as td from "../..";
-import * as assert from "assert";
+import * as td from '../..'
+import * as assert from 'assert'
 
 class Dog {
-  bark() {}
+  bark () {}
 }
 
 class Cat {
-  meow(): string {
-    return "meow! meow!";
+  meow (): string {
+    return 'meow! meow!'
   }
 }
 
-function sum(first: number, second: number): number {
-  return first + second;
+function sum (first: number, second: number): number {
+  return first + second
 }
 
 export = {
-  "play with some typescript"() {
-    const dog = td.constructor(Dog);
-    td.when(dog.prototype.bark()).thenReturn("woof!");
+  'play with some typescript' () {
+    const dog = td.constructor(Dog)
+    td.when(dog.prototype.bark()).thenReturn('woof!')
 
     const bird = td.object({
-      fly: function(): string {
-        return "swoosh";
+      fly: function (): string {
+        return 'swoosh'
       }
-    });
-    td.when(bird.fly()).thenReturn("swoosh!");
+    })
+    td.when(bird.fly()).thenReturn('swoosh!')
 
     const fish = td.object<{
       swim(): { speed: number; direction: number };
-    }>();
-    td.when(fish.swim()).thenReturn({ speed: 100 });
+    }>()
+    td.when(fish.swim()).thenReturn({ speed: 100 })
 
-    const kitty = td.object(["scratch", "meow"]);
-    td.when(kitty.scratch()).thenReturn("scratch!");
-    td.when(kitty.meow()).thenReturn("meow!");
+    const kitty = td.object(['scratch', 'meow'])
+    td.when(kitty.scratch()).thenReturn('scratch!')
+    td.when(kitty.meow()).thenReturn('meow!')
 
+    // eslint-disable-next-line
     if (eval("typeof Proxy") !== "undefined") {
-      // eslint-disable-line
       class Bear {
-        sleep(): string {
-          return "test";
+        sleep (): string {
+          return 'test'
         }
       }
 
-      const FakeBear = td.constructor<Bear>(Bear);
+      const FakeBear = td.constructor<Bear>(Bear)
 
       assert.strictEqual(
         td.explain(FakeBear.prototype.sleep).isTestDouble,
         true
-      );
+      )
 
-      const bear = td.object<Bear>("A bear");
+      const bear = td.object<Bear>('A bear')
 
-      td.when(bear.sleep()).thenReturn("zzzzzz");
+      td.when(bear.sleep()).thenReturn('zzzzzz')
 
-      assert.strictEqual(bear.sleep(), "zzzzzz");
+      assert.strictEqual(bear.sleep(), 'zzzzzz')
     }
 
     const testObject = {
-      funk: function() {}
-    };
+      funk: function () {}
+    }
 
-    td.replace(testObject, "funk");
-    td.replace(testObject, "funk", () => 42);
-    td.replace("../..");
-    td.replace("../../", 42);
+    td.replace(testObject, 'funk')
+    td.replace(testObject, 'funk', () => 42)
+    td.replace('../..')
+    td.replace('../../', 42)
 
-    const f = td.function();
-    td.when(f(10)).thenReturn(10, 11);
-    td.when(f(1)).thenThrow(new Error("ok"));
-    td.when(f(td.matchers.isA(String))).thenDo(function(s: string) {
-      return s;
-    });
-    td.when(f(td.matchers.not(true))).thenResolve("value1", "value2");
-    td.when(f(td.matchers.not(false))).thenReject(new Error("rejected"));
+    const f = td.function()
+    td.when(f(10)).thenReturn(10, 11)
+    td.when(f(1)).thenThrow(new Error('ok'))
+    td.when(f(td.matchers.isA(String))).thenDo(function (s: string) {
+      return s
+    })
+    td.when(f(td.matchers.not(true))).thenResolve('value1', 'value2')
+    td.when(f(td.matchers.not(false))).thenReject(new Error('rejected'))
 
-    const fakeSum = td.function(sum);
-    td.when(fakeSum(1, 2)).thenReturn(3);
+    const fakeSum = td.function(sum)
+    td.when(fakeSum(1, 2)).thenReturn(3)
 
-    const fakestSum = td.function("sum");
-    td.when(fakestSum(1, 2)).thenReturn(3);
+    const fakestSum = td.function('sum')
+    td.when(fakestSum(1, 2)).thenReturn(3)
 
-    f();
-    td.verify(f());
-    td.verify(f(), { times: 1 });
-    td.verify(f(), { ignoreExtraArgs: false });
-    td.verify(f(), { ignoreExtraArgs: true, times: 1 });
+    f()
+    td.verify(f())
+    td.verify(f(), { times: 1 })
+    td.verify(f(), { ignoreExtraArgs: false })
+    td.verify(f(), { ignoreExtraArgs: true, times: 1 })
 
-    const CatFake = td.constructor(Cat);
-    const cat = new CatFake();
-    td.when(cat.meow()).thenReturn("moo!");
+    const CatFake = td.constructor(Cat)
+    const cat = new CatFake()
+    td.when(cat.meow()).thenReturn('moo!')
 
-    const explanation = td.explain(f);
+    const explanation = td.explain(f)
 
     assert.strictEqual(
-      explanation.description.split("\n")[0],
-      "This test double has 5 stubbings and 1 invocations."
-    );
+      explanation.description.split('\n')[0],
+      'This test double has 5 stubbings and 1 invocations.'
+    )
   }
 };
